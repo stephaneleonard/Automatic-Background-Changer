6050727a9e531803290d6c3555bdeef02b2dfd88
stephaneleonard <stefleonard97@gmail.com>
2021-09-20 11:54:02 +0200

    added a small text over the image

--- background_changer/background_changer.py
+++ background_changer/background_changer.py
@@ -1,4 +1,8 @@
 import ctypes
+import os
 
-def updateBackground():
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, "C:/Users/stefl/Documents/Automatic-Background-Changer/sample_image.png" , 0)
+def update_background(system):
+    if system == 'Windows':
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, "C:/Users/stefl/Documents/Automatic-Background-Changer/sample_image.png" , 0)
+    elif system == 'Linux':
+        os.system("gsettings set org.gnome.desktop.background picture-uri file:/home/stephane/Documents/projet\ perso/Automatic-Background-Changer/sample_image.png")
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from background_changer.background_changer import updateBackground
+from background_changer.background_changer import update_background
 from photo_retriever.abstract_photo_retriever import NasaPhotoRetriever
+from Image_resizer.image_resizer import resize_image
 
 
 def main():
     nasa = NasaPhotoRetriever()
     infos = nasa.retrievePictureInfos()
     nasa.downloadPicture(infos)
-    updateBackground()
+    resize_image(1920, 1080, infos['title'])
+    update_background('Linux')
 
 if __name__ == "__main__":
     main()
